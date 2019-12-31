c7cfda5bf56a4c34b2dc672afc9fbe0cdcfb0fb7
Yusuke Izawa <yuizalp@gmail.com>
2020-01-01 03:09:21 +0900

    Change directory scructure (many breaking changes!)

--- benchmark/shootout/harmonic.ml
+++ benchmark/shootout/harmonic.ml
@@ -1,7 +1,7 @@
 (*MINCAML*) let rec f n i d s =
 (*MINCAML*)   if i > n then s else
-(*MINCAML*)   f n (i + 1) (d +. 1.0) (s +. 1.0 /. d) in
-(*MINCAML*) print_int (int_of_float (1000000.0 *. f 100000000 2 2.0 1.0));
+(*MINCAML*)   f n (i + 1) (d + 1) (s + 1 - d) in
+(*MINCAML*) print_int ((1000000 * f 100000000 2 2 1));
 (*NOMINCAML let f n = *)
 (*NOMINCAML   let s = ref 1.0 in *)
 (*NOMINCAML   let d = ref 2.0 in *)
