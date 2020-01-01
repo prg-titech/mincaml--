41af9ce8db3b9547e28a0f4cb9b88957de81cd40
Yusuke Izawa <yuizalp@gmail.com>
2020-01-01 20:52:15 +0900

    Fix test programs

--- test/matmul-flat-int.ml
+++ test/matmul-flat-int.ml
@@ -5,11 +5,11 @@ let rec loop3 i k j a b c =
    loop3 i (k - 1) j a b c) in
 let rec loop2 i m j a b c =
   if j < 0 then 1 else
-  (let d = loop3 i (m - 1) j a b c in
+  (let _ = loop3 i (m - 1) j a b c in
    loop2 i m (j - 1) a b c) in
 let rec loop1 i m n a b c =
   if i < 0 then 0 else
-  (let d = loop2 i m (n - 1) a b c in
+  (let _ = loop2 i m (n - 1) a b c in
    loop1 (i - 1) m n a b c) in
 let rec mul l m n a b c =
   loop1 (l - 1) m n a b c in
@@ -20,7 +20,7 @@ let rec init i n mat =
     init (i - 1) n mat) in
 let rec make m n dummy =
   let mat = Array.make m dummy in
-  let d = init (m - 1) n mat in
+  let _ = init (m - 1) n mat in
   mat in
 let a = make 2 3 dummy in
 let b = make 3 2 dummy in
@@ -30,5 +30,5 @@ a.(1).(0) <- 4; a.(1).(1) <- 5; a.(1).(2) <- 6;
 b.(0).(0) <- 7; b.(0).(1) <- 8;
 b.(1).(0) <- 9; b.(1).(1) <- 10;
 b.(2).(0) <- 1; b.(2).(1) <- 12;
-let d = mul 2 3 2 a b c in
+let _ = mul 2 3 2 a b c in
 print_int ((c.(0).(0)) + c.(0).(1) + c.(1).(0) + c.(1).(1))
