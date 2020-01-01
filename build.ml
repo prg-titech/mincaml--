c9ee5727ffb6525788bc3d2e2a8aa549afad6814
Yusuke Izawa <yuizalp@gmail.com>
2020-01-02 00:46:24 +0900

    Fix build.ml

--- build.ml
+++ build.ml
@@ -11,7 +11,8 @@ let () =
   else if F.check_suffix target ".bc" then
     let f = F.chop_extension target in
     if cmd (s "dune exec min-caml -- %s" target) = 0 then
-      ignore (cmd(s "gcc -m32 -O2 -Wall src/stub.c src/libmincaml.S %s -lm -o %s" (f ^ ".s") f))
+      ignore (cmd (
+        s "gcc -m32 -O2 -Wall src/stub.c src/libmincaml.S %s -lm -o %s" (f ^ ".s") (f ^ ".exe")))
     else
       failwith "compilation failed."
   else ()
