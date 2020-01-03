9146118f5e29c48a0028dee6553f4d0956608603
Yusuke Izawa <yuizalp@gmail.com>
2020-01-04 04:02:10 +0900

    Update build.ml

--- build.ml
+++ build.ml
@@ -7,11 +7,9 @@ let cmd = Sys.command
 
 let () =
   let target = Sys.argv.(1) in
-  if F.check_suffix target ".bc" then
-    let f = F.chop_extension target in
-    if cmd (s "dune exec min-caml -- %s" target) = 0 then
-      ignore (cmd (
-        s "gcc -m32 -O2 -Wall src/stub.c src/libmincaml.S %s -lm -o %s" (f ^ ".s") (f ^ ".exe")))
-    else
-      failwith "compilation failed."
-  else ()
+  let f = F.chop_extension target in
+  if cmd (s "dune exec min-caml -- %s" target) = 0 then
+    ignore (cmd (
+      s "gcc -m32 -O2 -Wall src/stub.c src/libmincaml.S %s -lm -o %s" (f ^ ".s") (f ^ ".exe")))
+  else
+    failwith "compilation failed."
