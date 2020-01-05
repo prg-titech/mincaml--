f9a6b3e65da62d4535794f2cf2cf44e4f86834b4
Yusuke Izawa <yuizalp@gmail.com>
2020-01-06 01:06:41 +0900

    Added tak.ml

---
+++ test/tak.ml
@@ -0,0 +1,15 @@
+(*
+ * Tak function to measure the performance
+ *   https://en.wikipedia.org/wiki/Tak_(function)
+ *
+ * This is a very lower level benchmark only using recursion.
+ *
+ * Note:
+ * After passing LLVM optimization passes, the outer tak() function call is transformed to a loop.
+ *)
+
+let rec tak x y z =
+  if x < y + 1 then y else
+  tak (tak (x-1) y z) (tak(y-1) z x) (tak(z-1) x y)
+in
+print_int (tak 12 6 0)
