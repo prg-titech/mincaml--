de4dd41ded646ce66a1eacd4c5e2d43b8cdf1151
Yusuke Izawa <yuizalp@gmail.com>
2020-01-06 01:03:11 +0900

    Fix bubble_sort.ml

--- test/bubble_sort.ml
+++ test/bubble_sort.ml
@@ -1,10 +1,10 @@
 let rec swap xs idx max =
-  if max - 1 <= idx
-  then ()
+  if max < idx + 1
+  then xs
   else (
     let l = xs.(idx) in
     let r = xs.(idx + 1) in
-    if l <= r
+    if l < r + 1
     then swap xs (idx + 1) max
     else (
       xs.(idx) <- r;
@@ -12,10 +12,10 @@ let rec swap xs idx max =
       swap xs (idx + 1) max))
 in
 let rec bubble_sort xs idx len =
-  if idx >= len - 1
+  if len - 1 < idx
   then xs
   else (
-    swap xs 0 (len - idx);
+    let xs = swap xs 0 (len - idx) in
     bubble_sort xs (idx + 1) len)
 in
 let a = Array.make 3 0 in
@@ -23,6 +23,4 @@ a.(0) <- 3;
 a.(1) <- 1;
 a.(2) <- 2;
 let a = bubble_sort a 0 3 in
-print_int (a.(0)); print_newline ();
-print_int (a.(1)); print_newline ();
-print_int (a.(2)); print_newline ()
+print_int (a.(2))
