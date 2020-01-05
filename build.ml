e957168c681ccb6080cb67006e8fd1fbeba56567
Yusuke Izawa <yuizalp@gmail.com>
2020-01-06 08:51:37 +0900

    Update build.ml

--- build.ml
+++ build.ml
@@ -3,11 +3,13 @@
 open Printf
 module F = Filename
 
+let byte_flg = ref false
+
 let s = sprintf
 let cmd = Sys.command
 let with_error_code f = if f () = 0 then () else raise Exit
 let emit_x86 f = cmd @@ s "dune exec min-caml -- %s" f
-let emit_bytecode f = cmd @@ s "dune exec bytegen -- %s" f
+let emit_bytecode f = cmd @@ s "dune exec bac-caml -- %s" f
 
 let compile_with_gcc f =
   cmd
@@ -17,23 +19,24 @@ let compile_with_gcc f =
        (f ^ ".exe")
 ;;
 
-let usage () = print_string @@ Sys.argv.(0) ^ " mode file"
+let usage = Sys.argv.(0) ^ " [-byte] file"
 
 let () =
-  let mode = String.trim Sys.argv.(1) in
-  let file = String.trim Sys.argv.(2) in
-  let name, ext = F.chop_extension file, F.extension file in
-  if mode = "gcc"
+  let file = ref "" in
+  Arg.parse
+    [ ("-byte", Arg.Unit (fun _ -> byte_flg := true), "") ]
+    (fun f -> file := f)
+    usage;
+  let name, ext = F.chop_extension !file, F.extension !file in
+  if not (!byte_flg)
   then (
-    with_error_code (fun () -> emit_x86 file);
+    with_error_code (fun () -> emit_x86 !file);
     with_error_code (fun () -> compile_with_gcc name))
-  else if mode = "byte"
-  then (
+  else (
     match ext with
     | ".mcml" ->
-      with_error_code (fun () -> emit_x86 file);
+      with_error_code (fun () -> emit_x86 !file);
       with_error_code (fun () -> compile_with_gcc name)
-    | ".ml" -> with_error_code (fun _ -> emit_bytecode file)
-    | _ -> usage ())
-  else usage ()
+    | ".ml" -> with_error_code (fun _ -> emit_bytecode !file)
+    | _ -> print_endline usage)
 ;;
