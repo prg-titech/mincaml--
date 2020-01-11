a9c4c576a6011f708fb23b55b740a4b4f10ebd81
Yusuke Izawa <yuizalp@gmail.com>
2020-01-12 06:39:41 +0900

    Update test programs

--- test/array.ml
+++ test/array.ml
@@ -1,12 +1,13 @@
-let rec ary i n xs ys =
-  if n < i
-  then ys
+let rec ary i n xs ys zs =
+  if n <= i
+  then zs
   else (
-    ys.(i) <- xs.(i) + ys.(i);
-    ary (i + 1) n xs ys)
+    zs.(i) <- xs.(i) + ys.(i);
+    ary (i + 1) n xs ys zs)
 in
 let n = 5000 in
 let xs = Array.make n 1 in
 let ys = Array.make n 2 in
-let _ = ary 0 n xs ys in
-print_int ys.(1)
+let zs = Array.make n 3 in
+let _ = ary 0 n xs ys zs in
+()
