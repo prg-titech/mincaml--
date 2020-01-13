a091473a4a19a66894a124aef0dfbbacc24785eb
Yusuke Izawa <yuizalp@gmail.com>
2020-01-14 03:47:29 +0900

    Add fib-sum and sum-fib

--- test/simple3.ml
+++ test/simple3.ml
@@ -2,6 +2,6 @@ let%tj rec sum n acc =
   if n < 2 then acc else
     sum (n-1) (n+acc) in
 let%mj rec fib n =
-  if n < 2 then sum 1000 0 else
+  if n < 2 then sum 10000 0 else
     fib (n-1) + fib (n-2) in
-print_int (fib 30)
+print_int (fib 20)
