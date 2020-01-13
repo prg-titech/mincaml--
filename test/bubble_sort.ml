97fc970871209dceae7055d60ccfe6d28e6d8c15
Yusuke Izawa <yuizalp@gmail.com>
2020-01-13 11:48:38 +0900

    Update.

--- test/bubble_sort.ml
+++ test/bubble_sort.ml
@@ -1,4 +1,4 @@
-let%mj rec swap xs idx max =
+let%tj rec swap xs idx max =
   if max < idx + 1
   then xs
   else (
@@ -22,7 +22,8 @@ let rec make_arr n arr =
   if n < 1 then arr else
     (arr.(n-1) <- rand_int 2048; make_arr (n-1) arr)
 in
-let a = Array.make 3 0 in
+let a = Array.make 10 0 in
 a.(0) <- 3; a.(1) <- 1; a.(2) <- 2;
-let b = bubble_sort a 0 3 in
+a.(3) <- 10; a.(4) <- (-1); a.(5) <- 8;
+let b = bubble_sort a 0 10 in
 print_int (a.(0))
