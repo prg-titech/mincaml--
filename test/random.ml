21d7a00892c0f30f6dbe42e4e5875b428dbfccd3
Yusuke Izawa <yuizalp@gmail.com>
2020-01-22 00:57:19 +0900

    Fix random

--- test/random.ml
+++ test/random.ml
@@ -1,14 +1,16 @@
-let rec gen_random max =
-  let last = 47 in
+let rec loop max i ran =
   let im = 13998 in
   let ia = 3877 in
   let ic = 29573 in
-  let last = ((last * ia) + ic) mod im in
-  (max * last) / im in
-let rec loop n i acc =
-  if i < n then
-    let x = gen_random i in
-    loop n (i+1) (acc+x)
-  else acc
+  if i <= 2 then 0 else
+    let last = ran.(0) in
+    let last = ((last * ia) + ic) mod im in
+    let new_last = (max * last) / im in
+    ran.(0) <- new_last;
+    loop max (i-1) ran
 in
-print_int (loop 100000000 1 1)
+let n = 1000 in
+let max = 100 in
+let ran = Array.make 42 2 in
+let _ = (loop max n ran) in
+()
