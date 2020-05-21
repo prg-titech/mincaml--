7ebefd7ac837b64e0ddab58354e7570c3aa16829
Yusuke Izawa <yuizalp@gmail.com>
2020-05-21 21:46:47 +0900

    Fixed sieve.ml

--- test/sieve.ml
+++ test/sieve.ml
@@ -1,17 +1,17 @@
-let%mj rec loop1 flags k i =
+let%tj rec loop1 flags k i =
   if k <= 8192 then  (* MJ k <= 4000 *)
     (flags.(k) <- (-1);
      loop1 flags (k+i) i)
   else flags in
-let rec sieve flags i =
+let%tj rec sieve flags i =
   if i <= 8192 then (* MJ k <= 4000 *)
-    if flags.(i) > 0 then
+    if flags.(i) > (-1) then
       let _ = loop1 flags (i+1) i in
       sieve flags (i+1)
     else
       sieve flags (i+1)
   else
     flags in
-let flags = Array.make 8196 1 in
-let _ = sieve flags 2 in
+let flags = Array.make 8193 1 in
+let _ = sieve flags 1 in
 ()
