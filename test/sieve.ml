9de0bcaf3b9e1af6b955b26b38e4bd75e24104a1
Yusuke Izawa <yuizalp@gmail.com>
2020-05-29 03:52:39 +0900

    Updated sieve.ml

--- test/sieve.ml
+++ test/sieve.ml
@@ -1,12 +1,12 @@
 let%tj rec loop1 flags k i =
-  if k <= 8192 then  (* MJ k <= 4000 *)
+  if k <= 8192 then
     (flags.(k) <- (-1);
      loop1 flags (k+i) i)
   else flags in
 let%tj rec sieve flags i =
-  if i <= 8192 then (* MJ k <= 4000 *)
-    if (-1) < flags.(i) then
-      let _ = loop1 flags (i+i) i in
+  if i <= 8192 then
+    if flags.(i) > (-1) then
+      let flags = loop1 flags (i+i) i in
       sieve flags (i+1)
     else
       sieve flags (i+1)
@@ -14,6 +14,6 @@ let%tj rec sieve flags i =
     flags in
 let flags = Array.make 8192 1 in
 flags.(0) <- (-1); flags.(1) <- (-1);
-let _ = sieve flags 1 in
-print_int (flags.(23));
+let _ = sieve flags 2 in
+print_int (flags.(11));
 ()
