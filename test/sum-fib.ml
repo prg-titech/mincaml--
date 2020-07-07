452ca2b2a31fbaea631a257b8e4f51e5fb3083a9
Yusuke Izawa <yuizalp@gmail.com>
2020-07-07 16:56:21 +0900

    Addes sum-fib

--- test/sum-fib.ml
+++ test/sum-fib.ml
@@ -3,6 +3,6 @@ let%mj rec fib n =
     fib (n-1) + fib (n-2) in
 let%tj rec sum i n =
   if i <= 1 then n else
-    let m = fib i in
+    let m = fib 10 in
     sum (i-1) (n + m) in
-print_int (sum 20 0)
+print_int (sum 100 0)
