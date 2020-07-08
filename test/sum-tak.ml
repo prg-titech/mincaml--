42854a5cb2a0c487acc3f6f2720de89e6a8cae6e
Yusuke Izawa <yuizalp@gmail.com>
2020-07-08 20:47:11 +0900

    Fixed.

--- test/sum-tak.ml
+++ test/sum-tak.ml
@@ -2,10 +2,10 @@ let%mj rec tak x y z =
   if x <= y then z else
     tak (tak (x-1) y z) (tak(y-1) z x) (tak(z-1) x y)
 in
-let%tj rec sum n =
-  let m = tak 12 6 4 in
-  if i <= 1 then m else
-    m + sum (i-1) n
+let%tj rec sum i n =
+  if i <= 1 then n else
+    let m = tak 12 6 4 in
+    sum (i-1) (n + m)
 in
-let _ = sum 100 in
+let r = sum 100 0 in
 ()
